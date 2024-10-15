cc45144b78c82a1be9ee01d06123683da903e0c5
Lukas Wigger <lukas@wigger.dev>
2024-10-15 10:59:05 +0200

    remove liveview tests

--- test/support/fixtures/achievements_fixtures.ex
+++ test/support/fixtures/achievements_fixtures.ex
@@ -11,7 +11,7 @@ defmodule Lernmit.AchievementsFixtures do
     {:ok, achievement} =
       attrs
       |> Enum.into(%{
-        desc: "",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida justo et nulla efficitur, maximus egestas sem pellentesque",
         image: "badge_CreatedFirstSet.svg",
         title: "Set builder"
       })
