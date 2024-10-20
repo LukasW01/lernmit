9386957c4cc77c3d53d0948c3e8021b904d2986f
Lukas Wigger <lukas@wigger.dev>
2024-10-20 22:56:23 +0200

    add Upcoming events section

--- lib/lernmit_web/live/calendar_live/week.ex
+++ lib/lernmit_web/live/calendar_live/week.ex
@@ -13,11 +13,6 @@ defmodule LernmitWeb.CalendarLive.Week do
      |> assign(:page_title, "Calendar")}
   end
 
-  @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:week_in_view, Date.utc_today())
@@ -29,6 +24,43 @@ defmodule LernmitWeb.CalendarLive.Week do
         Date.end_of_week(Date.utc_today())
       )
     )
+  end
+
+  def events(assigns) do
+    ~H"""
+    <section class="mt-12">
+      <h2 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">Upcoming events</h2>
+      <ol class="mt-2 divide-y divide-gray-200 dark:divide-gray-700 text-sm leading-6 text-gray-500 dark:text-gray-400">
+        <%= if Enum.empty?(@calendar) do %>
+          <li class="py-4 sm:flex">
+            <p class="mt-2 flex-auto sm:mt-0">Nothing on your schedule.</p>
+          </li>
+        <% end %>
+        <%= for task <- @calendar do %>
+          <li class="py-4 sm:flex">
+            <time datetime={Calendar.strftime(task.due_date, "%Y-%m-%d")} class="w-28 flex-none">
+              <%= Calendar.strftime(task.due_date, "%B %d") %>
+            </time>
+            <.link patch={~p"/task/#{task.id}"}>
+              <p class="mt-2 flex-auto font-semibold text-gray-900 dark:text-white sm:mt-0">
+                <%= task.title %>
+              </p>
+            </.link>
+            <span class="ml-auto flex-none md:block hidden">
+              <time datetime={Calendar.strftime(task.due_date, "%Y-%m-%d")}>
+                <%= Calendar.strftime(task.due_date, "%I:%M %p") %>
+              </time>
+            </span>
+          </li>
+        <% end %>
+      </ol>
+    </section>
+    """
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   @impl true
